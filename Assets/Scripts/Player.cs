031e78f2bc0366acc7caca1d74920e874ad77ced
Sofia Beltran <sofiabelmar@outlook.com>
2021-02-03 22:17:38 -0700

    animaciones y movimiento 3d

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        transform.Translate(Axis.normalized.magnitude * Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if(Axis != Vector3.zero){
+            transform.rotation = Quaternion.LookRotation(Axis.normalized);
+        }
+        anim.SetFloat("move", Mathf.Abs(Axis.normalized.magnitude));
+    }
+
+    Vector3 Axis
+    {
+        get=> new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+}
